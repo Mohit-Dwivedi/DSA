163c99e595f9df2c43a37f952c565b590e3f3b4e
Mohit Dwivedi <md8932537@gmail.com>
2024-10-07 23:14:18 +0530

    First commit

---
+++ pattern.java
@@ -0,0 +1,202 @@
+ public class Pattern {
+     public static void main(String[] args) {
+//         //pattern 1
+//         for(int i = 0; i<4; i++){
+//                 for(int j = 0; j<5; j++){
+//                         System.out.print("* ");
+//                 }
+//                 System.out.println("");
+//         } 
+
+        //pattern 2
+//        for(int i = 0; i<4; i++){
+//         for(int j = 0; j<5; j++){
+//                 if(i == 0 || i == 4-1 || j == 0 || j == 5-1){
+//                         System.out.print("*");
+//                 }
+//                 else{
+//                         System.out.print(" ");
+//                 }
+//         }
+//                 System.out.println("");
+//        }
+
+        // pattern 3
+//       for(int i = 1; i<=4; i++){
+//         for(int j = 1; j<=i; j++){
+//                 System.out.print("*");
+//         }
+//         System.out.println("");
+//       }
+
+
+        //pattern 4
+//        for(int i = 4; i >= 0; i--) {
+//            for(int j = 1; j <=i ; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        //pattern 5
+        // for(int i = 1 ; i<=4; i++){
+        //         for(int j = 1; j<=4-i; j++){
+        //                 System.out.print(" ");
+        //         }
+        //         for(int k = 1; k<=i; k++){
+        //                 System.out.print("*");
+        //         }
+        //         System.out.println("");
+        // }
+
+        //pattern 6
+        // for(int i = 1; i<=5; i++){
+        //         for(int j = 1; j<=i; j++){
+        //                 System.out.print(j);
+        //         }
+        //         System.out.println("");
+        // }
+
+
+        //pattern 7
+        //    for(int i = 5; i>=1; i--){
+        //         for(int j = 1; j<=i; j++){
+        //                 System.out.print(j);
+        //         }
+        //         System.out.println("");
+        //    }
+
+        //pattern 8 
+        // int num = 1;
+        // for(int i = 1; i<=5; i++){
+        //         for(int j = 1; j<=i; j++){
+        //                 System.out.print(num++);
+        //                 System.out.print(" ");
+        //         }
+        //         System.out.println("");
+        // }
+
+        // pattern 9 
+        // for(int i = 1; i<=5; i++){
+        //         for(int j = 1; j<=i; j++){
+        //                 if((i+j) % 2 == 0){
+        //                         System.out.print(1);
+        //                 }
+        //                 else{
+        //                         System.out.print(0);
+        //                 }
+        //         }
+        //         System.out.println("");
+        // }
+
+        //pattern 10
+//        for(int i = 1; i <=4; i++) {
+//            for(int j = 1; j <=i; j++) {
+//                System.out.print("*");
+//            }
+//            int spaces = 2 * (4-i);
+//            for(int j = 1; j <=spaces; j++) {
+//                System.out.print(" ");
+//            }
+//            for(int j = 1; j <=i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for(int i = 4; i >= 0; i--) {
+//            for(int n = 1; n <=i; n++) {
+//                System.out.print("*");
+//            }
+//            int spaces = 2 * (4-i);
+//            for(int j = 1; j <=spaces; j++) {
+//                System.out.print(" ");
+//            }
+//            for(int j = 1; j <=i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        //pattern 11
+        // for(int i = 0; i<=5; i++){
+        //         for(int j = 1; j<=5-i; j++){
+        //                 System.out.print(" ");
+        //         }
+        //         for(int k = 1; k<=5; k++){
+        //                 System.out.print("*");
+        //         }
+        //         System.out.println("");
+        // }
+
+        //pattern 12
+//        int n =5;
+//        for (int i = 1; i <= n; i++){
+//            for(int j = 1; j <= n-i; j++){
+//                System.out.print(" ");
+//            }
+//            for(int j = 1; j <= i; j++){
+//                System.out.print(i);
+//                System.out.print(" ");
+//            }
+//            System.out.println();
+//        }
+
+        //pattern 13
+//        int n =5;
+//        for (int i = 1; i <= n; i++){
+//            for(int j = 1; j <= n-i; j++){
+//                System.out.print(" ");
+//            }
+//            for(int j = i; j >= 1; j--){
+//                System.out.print(j);
+//            }
+//            for(int j = 2; j <= i; j++){
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+
+        //pattern 14
+//        int n = 4;
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= n-i; j++) {
+//                System.out.print(" ");
+//            }
+//            for (int k = 1; k <= 2*i-1; k++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for(int m = n; m >= 1; m--){
+//            for (int j = 0; j < n-m; j++) {
+//                System.out.print(" ");
+//            }
+//            for (int k = 1; k <= 2*m-1; k++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        //pattern 15
+//        for(int i = 1; i <= 4; i++){
+//            for(int j = i; j <= 4; j++){
+//                System.out.print(i + " ");
+//            }
+//            System.out.println();
+//        }
+
+        //pattern 16
+//        for(int i = 1; i <= 5; i++){
+//            for(int j = 1; j <= 5-i; j++){
+//                System.out.print(" ");
+//            }
+//            for(int j = 1; j <= i; j++){
+//                System.out.print(j);
+//                System.out.print(" ");
+//            }
+//            System.out.println();
+//        }
+//
+    }
+}
+
